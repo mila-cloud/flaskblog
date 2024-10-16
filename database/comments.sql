1cb65f4e5b399aa37c07f8a05f24c1bd89e221e7
mila-cloud <camila.azeveddo92@gmail.com>
2024-10-16 11:44:50 -0300

    AULA17

---
+++ database/comments.sql
@@ -0,0 +1,6 @@
+SELECT com_author_name, com_comment,
+        DATE_FORMAT(com_date, '%d/%m/%Y às %H:%i') AS com_datebr
+        FROM comment
+        WHERE com_article = '10'
+            AND com_status = 'on'
+        ORDER BY com_date DESC;
